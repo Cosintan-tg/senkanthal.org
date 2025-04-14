46a8cdee1595a030c313b04dcd4c85f5e9c6d460
sivothajan <sivothajan2002@gmail.com>
2025-04-15 00:02:02 +0530

    Add Loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Loading from './pages/loading/Loading';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
+  </StrictMode>
 )
---
+++ src/pages/loading/Loading.jsx
@@ -0,0 +1,9 @@
+import styles from "./Loading.module.css";
+
+function Loading() {
+    return (
+        <div className={styles.loading} />
+    );
+}
+
+export default Loading;
